fa1d480449c007428a74a24b6803dcbb94978f93
Luis Leal <luisleal.sistemas@gmail.com>
2021-11-13 02:17:30 -0600

    Diseño de login

--- src/components/LaunchScreen.js
+++ src/components/LaunchScreen.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { signIn } from '../actions';
 
-
 class LaunchScreen extends React.Component {
   constructor(props) {
     super(props);
@@ -16,13 +15,16 @@ class LaunchScreen extends React.Component {
       } else {
         this.props.history.push('/login');
       }
-    }, 1000);
+    }, 4000);
   }
 
   render() {
     return (
-      <div>
-        <h3>Launch Screen</h3>
+      <div className="min-h-screen flex items-center justify-center red-color">
+        <div className="w-1/2 sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3">
+            <img className="fade-in" src={'logo.png'} />
+            <div className="loader" />
+        </div>
       </div>
     );
   }
--- src/components/Login.js
+++ src/components/Login.js
@@ -21,11 +21,34 @@ class Login extends React.Component {
   }
 
   render() {
-    console.log(this.props.isSignedIn);
     return (
       <div>
-        <h3>Login</h3>
-        <button onClick={this.onSubmit}>login</button>
+        <div className="red-color">
+          <div className="flex items-center justify-center h-40 md:h-80 object-center ">
+            <div className="w-1/2 sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3">
+              <img src={'logo.png'} />
+            </div>
+          </div>
+        </div>
+        <div className="container mx-auto px-4">
+          <div className="flex items-center justify-center object-center ">
+            <div className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3 mt-5">
+              <input placeholder="Correo" className="w-full pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200" type="text"/>
+            </div>
+          </div>
+          <div className="flex items-center justify-center object-center ">
+            <div className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3 mt-5">
+              <input placeholder="Contraseña" className="w-full pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200" type="password"/>
+            </div>
+          </div>
+          <div className="flex items-center justify-center object-center ">
+            <div className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3 mt-5">
+              <button onClick={this.onSubmit} className="w-full h-10 px-6 red-color  rounded-full focus:shadow-outline transition duration-300 ease-in-out transform hover:scale-105 text-white font-semibold">
+                Entrar
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
