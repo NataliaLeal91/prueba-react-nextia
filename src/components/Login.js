e927320cb22444cc13828b4b8ab4bed074111002
Luis Leal <luisleal.sistemas@gmail.com>
2021-11-14 15:58:01 -0600

    Detalles faltantes de diseño en el login

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import { signIn } from '../actions';
 
-
 import ErrorAlert from '../components/ErrorAlert';
 
 class Login extends React.Component {
@@ -16,9 +15,12 @@ class Login extends React.Component {
     };
     
     this.handleOnChangeLogin = this.handleOnChangeLogin.bind(this); 
+    this.handleEnter = this.handleEnter.bind(this); 
   }  
 
-  onSubmit = () => {
+  onSubmit(e) {
+    e.preventDefault();
+
     const { username, password, completedForm } = this.state;
 
     if (completedForm) {
@@ -32,7 +34,6 @@ class Login extends React.Component {
     if (this.props.isSignedIn) {
       this.props.history.push('/benevits');
     }
-
 
     completedForm = (username && password); 
     
@@ -57,46 +58,70 @@ class Login extends React.Component {
     this.setState({ username, password, completedForm });
   }
 
+  handleEnter(e, type) {
+    if (e.key.toLowerCase() === "enter") {
+      const { username, password, completedForm } = this.state;
+
+      if (type === "password") {
+        if (completedForm) {
+          this.props.signIn(username, password);
+        }
+      } else {
+
+        const form = e.target.form;
+        const index = [...form].indexOf(e.target);
+        form.elements[index + 1].focus();
+
+      }
+
+      e.preventDefault();
+    }
+  }
+
   render() {
     return (
       <Fragment>
-        <div className="red-color">
-          <div className="flex items-center justify-center h-40 md:h-80 object-center ">
+        <div className="red-color bg-login-curved">
+          <div className="flex py-10 justify-center h-40 md:h-96 object-center ">
             <div className="w-1/2 sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3">
               <img src={'logo.png'} />
             </div>
           </div>
         </div>
         <div className="container mx-auto px-4">
-          <div className="flex items-center justify-center object-center ">
-            <div className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3 mt-5">
-              <input value={this.state.username}
-                placeholder="Correo"
-                className="w-full pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
-                type="email" required
-                onChange={(e) => this.handleOnChangeLogin(e, "username")}/>
+          <form>
+            <div className="flex items-center justify-center object-center ">
+              <div className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3 mt-5">
+                <input value={this.state.username}
+                  onKeyDown={(e) => this.handleEnter(e, 'username')}
+                  placeholder="Correo"
+                  className="w-full pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+                  type="email" required
+                  onChange={(e) => this.handleOnChangeLogin(e, "username")}/>
+              </div>
             </div>
-          </div>
-          <div className="flex items-center justify-center object-center ">
-            <div className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3 mt-5">
-              <input value={this.state.password}
-                placeholder="Contraseña"
-                className="w-full pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
-                type="password"
-                onChange={(e) => this.handleOnChangeLogin(e, "password")} />
+            <div className="flex items-center justify-center object-center ">
+              <div className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3 mt-5">
+                <input value={this.state.password}
+                  onKeyDown={(e) => this.handleEnter(e, 'password')}
+                  placeholder="Contraseña"
+                  className="w-full pt-3 pb-2 block w-full px-0 mt-0 bg-transparent border-0 border-b-2 appearance-none focus:outline-none focus:ring-0 focus:border-black border-gray-200"
+                  type="password"
+                  onChange={(e) => this.handleOnChangeLogin(e, "password")} />
+              </div>
             </div>
-          </div>
-          <div className="flex items-center justify-center object-center ">
-            <div className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3 mt-5">
-              <button
-                onClick={this.onSubmit}
-                className={`w-full h-10 px-6 red-color rounded-full focus:shadow-outline ${this.state.completedForm ? 'transition duration-300 ease-in-out transform hover:scale-105': 'gray-color cursor-not-allowed	'} text-white font-semibold`}>
-                Entrar
-              </button>
+            <div className="flex items-center justify-center object-center ">
+              <div className="w-full md:w-1/3 lg:w-1/3 xl:w-1/3 mt-5">
+                <button
+                  onClick={(e) => this.onSubmit(e)}
+                  className={`w-full h-10 px-6 red-color rounded-full focus:shadow-outline ${this.state.completedForm ? 'transition duration-300 ease-in-out transform hover:scale-105': 'gray-color cursor-not-allowed	'} text-white font-semibold`}>
+                  Entrar
+                </button>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
-        <ErrorAlert errorMessage={"Usuario y/o contraseña incorrectos. Favor de verificar su información."} />
+        <ErrorAlert errorMessage={"Usuario y/o contraseña incorrectos. Intenta Nuevamente"} />
       </Fragment>
     );
   }
