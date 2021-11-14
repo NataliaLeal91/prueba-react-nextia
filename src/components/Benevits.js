6e78f892cc625a4eed729a3d433c15ed6c4f6bf6
Luis Leal <luisleal.sistemas@gmail.com>
2021-11-14 00:32:01 -0600

    Integración de menu a la vista de Benevits

--- src/components/Benevits.js
+++ src/components/Benevits.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
 import {
   fetchWallets, fetchBenevits, signOut
@@ -26,8 +26,6 @@ class Benevits extends React.Component {
   }
 
   render() {
-    console.log(this.props.wallets);
-    console.log(this.props.benevits);
 
     // if(this.state.loading || !this.state || !this.state.data) {
     if(!this.props.benevits || !this.props.benevits.benevits) {
@@ -35,64 +33,121 @@ class Benevits extends React.Component {
     } else {
 
       return (
-        <div className="gray-color">
-            {this.props.wallets ?
-              this.props.wallets.wallets.map((wallet) => {
+        <Fragment>
+          <div className="shadow bg-base-200 drawer">
+            <input id="my-drawer" type="checkbox" className="drawer-toggle" /> 
+            <div className="flex flex-col items-center justify-center drawer-content gray-color" style={{maxHeight: 'none'}}>
 
-                let unlockedBenevits = this.props.benevits.benevits.unlocked.filter((benevit) => {
-                  return benevit.wallet.id === wallet.id
-                });
 
-                let lockedBenevits = this.props.benevits.benevits.locked.filter((benevit) => {
-                  return benevit.wallet.id === wallet.id
-                });
-                
-                return <div className="container" style={{margin: "0 auto"}}>
-                  <h1 className="font-bold py-4">{wallet.name}</h1>
 
-                  <div className="grid justify-items-center grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2">
+              <div class="w-full navbar red-color">
+                <div class="flex-none lg:hidden">
+                  <label for="my-drawer-3" class="btn btn-square btn-ghost">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                    </svg>
+                  </label>
+                </div> 
 
-                    {unlockedBenevits ? 
-                      unlockedBenevits.map((benevit) => {
-                        console.log("aqui esta", benevit);
-                        return <div className="rounded bg-white w-full" >
-                          <div className="flex h-20 rounded-t-lg " style={{ backgroundColor: benevit.primary_color }}>
-                            <img className="w-full" src={benevit.ally.mini_logo_full_path}/>
-                          </div>
-                          <div className="flex h-52 p-2">
-                            <div className="w-full">
-                              <p className="font-bold">{benevit.ally.name}</p>
-                              <p>{benevit.ally.description}</p>
-                              <p className="text-sm mt-3">{benevit.title}</p>
-                            </div>
-                          </div>
-                        </div>
-                      })
-                    : null}
-                  
-                    {lockedBenevits ?
-                      lockedBenevits.map((benevit) => {
-                        return <div className="p-2 rounded bg-white w-full">
-                            <div className="flex h-52">
-                              <img className="w-full m-auto" src={benevit.vector_full_path} />
-                            </div>
-                            <div className="flex h-20">
-                              <button
-                                onClick={this.onSubmit}
-                                className="w-full self-end h-10 px-6 red-color rounded-full focus:shadow-outline transition duration-300 ease-in-out transform hover:scale-105 text-white font-semibold">
-                                Lo quiero
-                              </button>
-                            </div>
-                        </div>
-                      })
-                    :null}
-                  </div>
+                <div class="flex-none hidden lg:block">
+                  <ul>
+                    <li>
+                      <label htmlFor="my-drawer" className="cursor-pointer" >
+                        <div className="menu-icon" />
+                        <div className="menu-icon" />
+                        <div className="menu-icon" />
+                      </label>
+                    </li> 
+                  </ul>
                 </div>
-              })
-            : null}
-          <hr />
-          {/* <button onClick={() => this.props.signOut()}>Cerrar sesión</button> */}
-        </div>
+
+                <div class="flex-1 px-2 mx-2">
+                  <span>
+                    <img className="h-16" src={'logo.png'} />
+                  </span>
+                </div> 
+              </div>
+ 
+
+
+
+
+              {this.props.wallets ?
+                this.props.wallets.wallets.map((wallet) => {
+
+                  let unlockedBenevits = this.props.benevits.benevits.unlocked.filter((benevit) => {
+                    return benevit.wallet.id === wallet.id
+                  });
+
+                  let lockedBenevits = this.props.benevits.benevits.locked.filter((benevit) => {
+                    return benevit.wallet.id === wallet.id
+                  });
+                  
+                  return <div className="container" style={{margin: "0 auto"}}>
+                    <h1 className="font-bold py-4">{wallet.name}</h1>
+
+                    <div className="grid justify-items-center grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-2">
+
+                      {unlockedBenevits ? 
+                        unlockedBenevits.map((benevit) => {
+                          return <div className="rounded bg-white w-full" >
+                            <div className="flex h-20 rounded-t-lg " style={{ backgroundColor: benevit.primary_color }}>
+                              <img className="w-full" src={benevit.ally.mini_logo_full_path}/>
+                            </div>
+                            <div className="flex h-52 p-2">
+                              <div className="w-full">
+                                <p className="font-bold">{benevit.ally.name}</p>
+                                <p>{benevit.ally.description}</p>
+                                <p className="text-sm mt-3">{benevit.title}</p>
+                              </div>
+                            </div>
+                          </div>
+                        })
+                      : null}
+                    
+                      {lockedBenevits ?
+                        lockedBenevits.map((benevit) => {
+                          return <div className="p-2 rounded bg-white w-full">
+                              <div className="flex h-52">
+                                <img className="w-full m-auto" src={benevit.vector_full_path} />
+                              </div>
+                              <div className="flex h-20">
+                                <button
+                                  onClick={this.onSubmit}
+                                  className="w-full self-end h-10 px-6 red-color rounded-full focus:shadow-outline transition duration-300 ease-in-out transform hover:scale-105 text-white font-semibold">
+                                  Lo quiero
+                                </button>
+                              </div>
+                          </div>
+                        })
+                      :null}
+                    </div>
+                  </div>
+                })
+              : null}
+
+            </div> 
+
+            <div className="drawer-side">
+              <label htmlFor="my-drawer" className="drawer-overlay" style={{position: 'fixed', width: '100%', height: '100vh'}}></label> 
+              <ul className="menu p-4 overflow-y-auto w-80 red-color text-base-content" style={{position: 'fixed', height: '100vh'}}>
+                <li>
+                  <img src={'logo.png'} />
+                </li>
+                <li>
+                  <button className="h-20 text-white text-left cursor-pointer font-bold" onClick={() => {
+                    this.props.history.push('/benevits');
+                  }}>Benevits</button>
+                </li>
+                <li style={{position: 'absolute', bottom: 0, width: '100%'}}>
+                  <button className="h-20 text-white text-left cursor-pointer font-bold" onClick={() => {
+                    this.props.signOut()
+                  }}>Cerrar sesión</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Fragment>
       );
     }
   }
